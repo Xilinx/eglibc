8c09a2c4b3a5df195d64ee98d225e2ee641c5898
nathan <nathan@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2007-06-13 12:20:20 +0000

    * sysdeps/unix/sysv/linux/powerpc/libc-start.c 	(__libc_start_main): Detect 8xx parts and clear 	__cache_line_size if detected. 	* sysdeps/unix/sysv/linux/powerpc/dl-sysdep.c 	(DL_PLATFORM_AUXV): Likewise.
    git-svn-id: http://www.eglibc.org/svn/trunk/libc@2503 7b3dc134-2b1b-0410-93df-9e9f96275f8d

--- sysdeps/unix/sysv/linux/powerpc/dl-sysdep.c
+++ sysdeps/unix/sysv/linux/powerpc/dl-sysdep.c
@@ -27,9 +27,21 @@ int __cache_line_size attribute_hidden;
 /* Scan the Aux Vector for the "Data Cache Block Size" entry.  If found
    verify that the static extern __cache_line_size is defined by checking
    for not NULL.  If it is defined then assign the cache block size
-   value to __cache_line_size.  */
+   value to __cache_line_size.  This is used by memset to
+   optimize setting to zero.  We have to detect 8xx processors, which
+   have buggy dcbz implementations that cannot report page faults
+   correctly.  That requires reading SPR, which is a privileged
+   operation.  Fortunately 2.2.18 and later emulates PowerPC mfspr
+   reads from the PVR register.   */
 #define DL_PLATFORM_AUXV						      \
       case AT_DCACHEBSIZE:						      \
+	if (__LINUX_KERNEL_VERSION >= 0x020218)				      \
+	  {								      \
+	    unsigned pvr = 0;						      \
+	    asm ("mfspr %0, 287" : "=r" (pvr));				      \
+	    if ((pvr & 0xffff0000) == 0x00500000)			      \
+	      break;							      \
+	  }								      \
 	__cache_line_size = av->a_un.a_val;				      \
 	break;
 
--- sysdeps/unix/sysv/linux/powerpc/libc-start.c
+++ sysdeps/unix/sysv/linux/powerpc/libc-start.c
@@ -109,11 +109,24 @@ int
       rtld_fini = NULL;
     }
 
-  /* Initialize the __cache_line_size variable from the aux vector.  */
+  /* Initialize the __cache_line_size variable from the aux vector.
+     This is used by memset to optimize setting to zero.  We have to
+     detect 8xx processors, which have buggy dcbz implementations that
+     cannot report page faults correctly.  That requires reading SPR,
+     which is a privileged operation.  Fortunately 2.2.18 and later
+     emulates PowerPC mfspr reads from the PVR register.  */
   for (ElfW (auxv_t) * av = auxvec; av->a_type != AT_NULL; ++av)
     switch (av->a_type)
       {
       case AT_DCACHEBSIZE:
+	if (__LINUX_KERNEL_VERSION >= 0x020218)
+	  {
+	    unsigned pvr = 0;
+	    
+	    asm ("mfspr %0, 287" : "=r" (pvr) :);
+	    if ((pvr & 0xffff0000) == 0x00500000)
+	      break;
+	  }
 	__cache_line_size = av->a_un.a_val;
 	break;
       }
