e8031e9af5d1a5e3ad33a5b79719962ecb3c59d7
jimb <jimb@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2007-10-24 20:52:07 +0000

    * bits/wchar.h (__WCHAR_MIN): Add missing underscores to name.
    git-svn-id: http://www.eglibc.org/svn/trunk/libc@3935 7b3dc134-2b1b-0410-93df-9e9f96275f8d

--- bits/wchar.h
+++ bits/wchar.h
@@ -30,14 +30,14 @@
 /* GCC may also define __WCHAR_UNSIGNED__.
    Use L'\0' to give the expression the correct (unsigned) type.  */
 #ifdef __WCHAR_UNSIGNED__
-#define WCHAR_MIN       L'\0'
+#define __WCHAR_MIN       L'\0'
 
 /* Failing that, rely on the preprocessor's knowledge of the
    signedness of wchar_t.  */
 #elif L'\0' - 1 > 0
-#define WCHAR_MIN       L'\0'
+#define __WCHAR_MIN       L'\0'
 #else
-#define WCHAR_MIN       (-WCHAR_MAX - 1)
+#define __WCHAR_MIN       (-__WCHAR_MAX - 1)
 #endif
 
 #endif	/* bits/wchar.h */
