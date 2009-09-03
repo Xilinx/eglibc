5071eb6db19390f0ad6349f03252e05f3192623d
joseph <joseph@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2009-09-03 14:33:58 +0000

    Merge changes between r8878 and r8915 from /fsf/trunk.
    git-svn-id: http://www.eglibc.org/svn/trunk/libc@8916 7b3dc134-2b1b-0410-93df-9e9f96275f8d

--- sysdeps/ieee754/dbl-64/wordsize-64/s_round.c
+++ sysdeps/ieee754/dbl-64/wordsize-64/s_round.c
@@ -1,5 +1,5 @@
-/* Round double value to long int.
-   Copyright (C) 1997, 2004, 2009 Free Software Foundation, Inc.
+/* Round double to integer away from zero.
+   Copyright (C) 1997, 2009 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1997.
 
@@ -18,63 +18,60 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
-#define llround __hidden_llround
-#define __llround __hidden___llround
-
 #include <math.h>
 
 #include "math_private.h"
 
 
-long int
-__lround (double x)
+static const double huge = 1.0e300;
+
+
+double
+__round (double x)
 {
-  int32_t j0;
-  int64_t i0;
-  long int result;
-  int sign;
+  int64_t i0, j0;
 
   EXTRACT_WORDS64 (i0, x);
   j0 = ((i0 >> 52) & 0x7ff) - 0x3ff;
-  sign = i0 < 0 ? -1 : 1;
-  i0 &= UINT64_C(0xfffffffffffff);
-  i0 |= UINT64_C(0x10000000000000);
-
-  if (j0 < (int32_t) (8 * sizeof (long int)) - 1)
+  if (__builtin_expect (j0 < 52, 1))
     {
       if (j0 < 0)
-	return j0 < -1 ? 0 : sign;
-      else if (j0 >= 52)
-	result = i0 << (j0 - 52);
+	{
+	  if (huge + x > 0.0)
+	    {
+	      i0 &= UINT64_C(0x8000000000000000);
+	      if (j0 == -1)
+		i0 |= UINT64_C(0x3ff0000000000000);
+	    }
+	}
       else
 	{
-	  i0 += UINT64_C(0x8000000000000) >> j0;
-
-	  result = i0 >> (52 - j0);
+	  uint64_t i = UINT64_C(0x000fffffffffffff) >> j0;
+	  if ((i0 & i) == 0)
+	    /* X is integral.  */
+	    return x;
+	  if (huge + x > 0.0)
+	    {
+	      /* Raise inexact if x != 0.  */
+	      i0 += UINT64_C(0x0008000000000000) >> j0;
+	      i0 &= ~i;
+	    }
 	}
     }
   else
     {
-      /* The number is too large.  It is left implementation defined
-	 what happens.  */
-      return (long int) x;
+      if (j0 == 0x400)
+	/* Inf or NaN.  */
+	return x + x;
+      else
+	return x;
     }
 
-  return sign * result;
+  INSERT_WORDS64 (x, i0);
+  return x;
 }
-
-weak_alias (__lround, lround)
+weak_alias (__round, round)
 #ifdef NO_LONG_DOUBLE
-strong_alias (__lround, __lroundl)
-weak_alias (__lround, lroundl)
-#endif
-
-/* long long has the same width as long on 64-bit machines.  */
-#undef llround
-#undef __llround
-strong_alias (__lround, __llround)
-weak_alias (__lround, llround)
-#ifdef NO_LONG_DOUBLE
-strong_alias (__lround, __llroundl)
-weak_alias (__lround, llroundl)
+strong_alias (__round, __roundl)
+weak_alias (__round, roundl)
 #endif
