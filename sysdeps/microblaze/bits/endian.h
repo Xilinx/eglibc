47072597e68f4251c605edb4b00d97d01c91e818
David Holsgrove <david.holsgrove@petalogix.com>
2012-03-01 15:28:08 +1000

    microblaze: Add definition for microblaze little endian byte order
    Signed-off-by: David Holsgrove <david.holsgrove@petalogix.com>

--- sysdeps/microblaze/bits/endian.h
+++ sysdeps/microblaze/bits/endian.h
@@ -4,4 +4,10 @@
 # error "Never use <bits/endian.h> directly; include <endian.h> instead."
 #endif
 
+#ifdef _BIG_ENDIAN
 #define __BYTE_ORDER __BIG_ENDIAN
+#define __FLOAT_WORD_ORDER __BIG_ENDIAN
+#else
+#define __BYTE_ORDER __LITTLE_ENDIAN
+#define __FLOAT_WORD_ORDER __LITTLE_ENDIAN
+#endif
