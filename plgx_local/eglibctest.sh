1e6a9a56bebbf3e2cb367898d9e4c447a6be7d62
David Holsgrove <david.holsgrove@petalogix.com>
2012-05-28 22:56:39 +1000

    plgx_local: Update tests
    Signed-off-by: David Holsgrove <david.holsgrove@petalogix.com>

--- plgx_local/eglibctest.sh
+++ plgx_local/eglibctest.sh
@@ -11,7 +11,7 @@ setup()
 	mkdir -p ${results_dir}
 	test_log=${results_dir}/test.$$.log
 	results_log=${results_dir}/test.$$.sum
-
+	STANDARD_LIBRARY_PATH=$LD_LIBRARY_PATH
 	if [ -z ${timeout} ];then
 		timeout=15
 	fi
@@ -21,15 +21,22 @@ main ()
 {
 	for test in $(cat ${test_list});
 	do
-		timeout -t ${timeout} ${test} >> ${test_log}
+		path=$(echo $test | sed 's|\.\/\(.*\/\).*|\/tmp\/build-libc-final/\1|g')
+		echo "---------------------" >> ${test_log}
+		echo "BEGIN TEST: ${test}" >> ${test_log}
+		LD_LIBRARY_PATH=${path}:$STANDARD_LIBRARY_PATH timeout -t ${timeout} ${test} >> ${test_log}
 		rc=$?
 		if [ "${rc}" == "0" ];then
 			echo "PASS: ${test}" | tee -a ${results_log}
+			echo "PASS" >> ${test_log}
 		elif [ "${rc}" == "143" ];then
 			echo "TIMEOUT: ${test}" | tee -a ${results_log}
+			echo "Timed out after ${timeout}s" >> ${test_log}
 		else
 			echo "FAIL: ${test}" | tee -a ${results_log}
+			echo "FAIL rc=${rc}" >> ${test_log}
 		fi
+		echo "END TEST: ${test}" >> ${test_log}
 	done
 
 	total_num=$(cat ${test_list} | wc -l )
@@ -43,11 +50,11 @@ main ()
 	echo " PetaLinux EGLIBC Test Run, $(date)" | tee -a ${results_log}
 	echo " target is $(hostname)" | tee -a ${results_log}
 	echo "" | tee -a ${results_log}
-	echo " # of unexpected failures  ${num_fail} " | tee -a ${results_log}
 	echo " # of expected passes      ${num_pass} " | tee -a ${results_log}
+	echo " # of unexpected failures  ${num_fail} " | tee -a ${results_log}
+	echo " # of unsupported tests    ${num_timeout} " | tee -a ${results_log}
 	echo "" | tee -a ${results_log}
 	echo " # total                   ${total_num}" | tee -a ${results_log}
-	echo " # timed out               ${num_timeout} " | tee -a ${results_log}
 	echo "" | tee -a ${results_log}
 	echo "-----------------------------------------------------------------------------" | tee -a ${results_log}
 	echo "" | tee -a ${results_log}
