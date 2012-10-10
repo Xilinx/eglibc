305af367e827eb5ec855ebbbcd59a58f05a3c2ee
joseph <joseph@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2012-10-10 21:20:58 +0000

    * elf/Makefile ($(objpfx)tst-rtld-load-self.out): Pass 	$(cross-test-wrapper) and $(..)scripts/run-with-env.sh to script. 	* elf/tst-rtld-load-self.sh: Use wrappers to run dynamic linker.
    git-svn-id: http://www.eglibc.org/svn/trunk/libc@21112 7b3dc134-2b1b-0410-93df-9e9f96275f8d

--- elf/tst-rtld-load-self.sh
+++ elf/tst-rtld-load-self.sh
@@ -21,25 +21,28 @@
 set -e
 
 rtld=$1
+cross_test_wrapper="$2"
+run_with_env="$3"
 result=0
 
 echo '# normal mode'
-$rtld $rtld 2>&1 && rc=0 || rc=$?
+${cross_test_wrapper} $rtld $rtld 2>&1 && rc=0 || rc=$?
 echo "# exit status $rc"
 test $rc -le 127 || result=1
 
 echo '# list mode'
-$rtld --list $rtld 2>&1 && rc=0 || rc=$?
+${cross_test_wrapper} $rtld --list $rtld 2>&1 && rc=0 || rc=$?
 echo "# exit status $rc"
 test $rc -eq 0 || result=1
 
 echo '# verify mode'
-$rtld --verify $rtld 2>&1 && rc=0 || rc=$?
+${cross_test_wrapper} $rtld --verify $rtld 2>&1 && rc=0 || rc=$?
 echo "# exit status $rc"
 test $rc -eq 2 || result=1
 
 echo '# trace mode'
-LD_TRACE_LOADED_OBJECTS=1 $rtld $rtld 2>&1 && rc=0 || rc=$?
+EGLIBC_LD_TRACE_LOADED_OBJECTS=1 ${cross_test_wrapper} ${run_with_env} \
+    $rtld $rtld 2>&1 && rc=0 || rc=$?
 echo "# exit status $rc"
 test $rc -eq 0 || result=1
 
